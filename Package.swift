b521594ba2a94315edec3a983feccc0a46194e65
Muhammad Amir Pervaiz <amir.pervaiz@quantuminventions.com>
2024-04-15 16:15:32 +0800

    commit

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OBUSDK",
+    name: "SPMTest",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
