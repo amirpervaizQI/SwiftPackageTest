c3fdccc625938292bab48da4d7cb89ed936687f6
Muhammad Amir Pervaiz <67275445+amirpervaizQI@users.noreply.github.com>
2024-04-16 00:27:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 5.10
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SPMTest",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "OBUSDK",
-            targets: ["OBUSDK"]),
+    name: "OBUSDK",
+    platforms: [
+        .iOS(.v13) // Adjust the iOS version as needed
     ],
-    dependencies: [
-            .package(url: "https://github.com/realm/realm-swift.git", from: "10.49.1") // Adjust the RealmSwift version as needed
-        ],
+    products: [
+        .library(
+            name: "OBUSDK", // This should match the module name of your framework
+            targets: ["OBUSDK"]
+        ),
+    ],
     targets: [
-        .binaryTarget(
+       .binaryTarget(
                    name: "OBUSDK",
                    url: "https://extol.mycloudrepo.io/public/repositories/extol-ios/extol-1.0.3.zip", // Replace with the URL to your XCFramework
                    checksum: "4327177fddef558ee67484bffa7c540bb29169dc6e341b8881f963f2e7a42530" // Replace with the checksum of your XCFramework
