429546ce6c6438899cdd879596fdb66c9a6f2c46
Muhammad Amir Pervaiz <amir.pervaiz@quantuminventions.com>
2024-07-25 15:15:31 +0800

    updated

--- File.swift
+++ File.swift
@@ -1,2 +1,10 @@
+//
+//  File.swift
+//  OBUSDK
+//
+//  Created by Muhammad Amir Pervaiz on 25/7/24.
+//
+
+
   url: "https://extol.mycloudrepo.io/public/repositories/obusdk-ios/extol-1.0.3-beta.40.zip", // Replace with the URL to your XCFramework
                    checksum: "bef7a0a78f690c32e6d9202a3379ca70a215351a0e913780c93cbd830f039bb4" // Replace with the checksum of your XCFramework
