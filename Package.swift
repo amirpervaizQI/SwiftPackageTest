c57d8272c6e3dfe9f7b44ff53bb4714543e4a32c
Muhammad Amir Pervaiz <67275445+amirpervaizQI@users.noreply.github.com>
2024-07-25 14:23:56 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
        .binaryTarget(
                    name: "OBUSDK",
                    url: "https://extol.mycloudrepo.io/public/repositories/obusdk-ios/extol-1.0.3-beta.40.zip", // Replace with the URL to your XCFramework
-                   checksum: "4327177fddef558ee67484bffa7c540bb29169dc6e341b8881f963f2e7a42530" // Replace with the checksum of your XCFramework
+                   checksum: "bef7a0a78f690c32e6d9202a3379ca70a215351a0e913780c93cbd830f039bb4" // Replace with the checksum of your XCFramework
                ),
     ]
 )
